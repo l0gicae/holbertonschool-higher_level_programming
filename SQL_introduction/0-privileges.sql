32b7970a07c00fb09e897c03ff04b73dbd1101fc
l0gicae <YOUR_EMAIL@example.com>
2025-12-24 08:35:41 +0000

    0. My privileges!

--- SQL_introduction/0-privileges.sql
+++ SQL_introduction/0-privileges.sql
@@ -1,3 +1,5 @@
 -- lists privileges for user_0d_1 and user_0d_2 on localhost
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
