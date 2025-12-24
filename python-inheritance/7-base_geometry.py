9806ab2ad0a4117b6a51339c62c1fea82ee50bb9
l0gicae <faridmehti@gmail.com>
2025-12-24 05:13:17 +0000

    Add BaseGeometry integer_validator and tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a BaseGeometry class."""
+"""BaseGeometry module."""
 
 
 class BaseGeometry:
-    """Base class for geometry objects."""
+    """BaseGeometry class."""
 
     def area(self):
-        """Raise an exception because area is not implemented."""
+        """Raise Exception because not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
+        """Validate value as a positive integer."""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
