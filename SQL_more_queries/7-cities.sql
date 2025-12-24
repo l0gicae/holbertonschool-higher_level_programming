cd157521285a60e8a661415571b7e9590da55c32
l0gicae <YOUR_EMAIL@example.com>
2025-12-24 10:00:36 +0000

    7. Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,17 @@
+-- creates database hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS cities (
+  id INT NOT NULL AUTO_INCREMENT,
+  state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
