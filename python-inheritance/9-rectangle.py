21ec45cbd5a919d7ffba79e584fab5602102cf52
l0gicae <faridmehti@gmail.com>
2025-12-24 05:13:18 +0000

    Task 9: Full rectangle with area and __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle with private width and height."""
+
+    def __init__(self, width, height):
+        """Initialize rectangle after validating width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
