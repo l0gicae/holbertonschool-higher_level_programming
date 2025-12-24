b01451140a5252e14ce3165c6431de0f1628997c
l0gicae <faridmehti@gmail.com>
2025-12-24 05:13:17 +0000

    Add Rectangle __repr__ for eval

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -44,10 +44,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        lines = []
-        for _ in range(self.__height):
-            lines.append("#" * self.__width)
-        return "\n".join(lines)
+        return "\n".join("#" * self.__width for _ in range(self.__height))
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
